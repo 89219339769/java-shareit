f28a095774826b0f0dff05c7b43536635973ec40
Slava <svyatoslav.rudakas@yandex.ru>
2022-12-07 14:41:48 +0300

    item patch ready

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -27,14 +27,12 @@ public class ItemService {
         return itemRepository.save(item);
     }
 
-    public Item updateItem(Long userId, Long itemId, Item item) {
+    public Item updateItem(Long itemId, Long userId, Item item) {
 
         item.setUserId(userId);
         item.setId(itemId);
 
         try {
-
-
             List<Item> clientItems = itemRepository.getItems().get(userId);
             for (int k = 0; k < clientItems.size(); k++) {
                 if (clientItems.get(k).getId() == item.getId()) {
@@ -44,15 +42,16 @@ public class ItemService {
                     }
                     if (item.getAvailable() != null & item.getAvailable() != uptadeItem.getAvailable()) {
                         uptadeItem.setAvailable(item.getAvailable());
-                        if (item.getDescription() != null & item.getDescription() != uptadeItem.getDescription()) {
+                    }
+                    if (item.getDescription() != null & item.getDescription() != uptadeItem.getDescription()) {
                             uptadeItem.setDescription(item.getDescription());
-                        }
+                    }
                         return uptadeItem;
                     }
                 }
-            }
-        } catch (NotFoundException e){
-            throw new NotFoundException("невозможно обновить, т.к. пользователя с этим номером не существует ");
+
+        } catch (RuntimeException e){
+            throw new NotFoundException("невозможно обновить, пользователя с этим номером не существует ");
         }
         throw new NotFoundException("невозможно обновить, т.к. пользователя с этим номером не существует ");
     }

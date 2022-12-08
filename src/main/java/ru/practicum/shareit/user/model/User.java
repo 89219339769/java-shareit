35b34af7a670eb9400cbed35e4e81f0c28c8ebc6
Slava <svyatoslav.rudakas@yandex.ru>
2022-12-08 20:52:13 +0300

    correct imports

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,11 +1,8 @@
 package ru.practicum.shareit.user.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+
 import lombok.Data;
 
-import javax.validation.constraints.Email;
 
 @Data
 public class User {

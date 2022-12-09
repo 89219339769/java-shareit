0fbf86f60d9b1b6c3241bd8b945982b309cf600c
Slava <svyatoslav.rudakas@yandex.ru>
2022-12-09 10:27:21 +0300

    correct code stile exeptions1

--- src/main/java/ru/practicum/shareit/exceptions/NoItemNameException.java
+++ src/main/java/ru/practicum/shareit/exceptions/NoItemNameException.java
@@ -1,6 +1,9 @@
 package ru.practicum.shareit.exceptions;
-    public class NoItemNameException extends RuntimeException {
-        public NoItemNameException(String message) { super(message); }
+
+public class NoItemNameException extends RuntimeException {
+    public NoItemNameException(String message) {
+        super(message);
     }
+}
 
 

ca057d3dc38944bff3b8b3ef8e47cea434b3fc65
Slava <svyatoslav.rudakas@yandex.ru>
2022-12-10 12:21:07 +0300

    corrects ready

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.practicum.shareit.Validator;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.item.model.Item;
+
 import java.util.List;
 
 @Service
@@ -28,28 +29,28 @@ public class ItemService {
     public Item updateItem(Long itemId, Long userId, Item item) {
         item.setUserId(userId);
         item.setId(itemId);
-        try {
-            List<Item> clientItems = itemRepository.getItems().get(userId);
-            for (int k = 0; k < clientItems.size(); k++) {
-                if (clientItems.get(k).getId().equals(item.getId())) {
-                    Item uptadeItem = clientItems.get(k);
-                    if (item.getName() != null && item.getName() != uptadeItem.getName()) {
-                        uptadeItem.setName(item.getName());
-                    }
-                    if (item.getAvailable() != null && item.getAvailable() != uptadeItem.getAvailable()) {
-                        uptadeItem.setAvailable(item.getAvailable());
-                    }
-                    if (item.getDescription() != null && item.getDescription() != uptadeItem.getDescription()) {
-                        uptadeItem.setDescription(item.getDescription());
-                    }
-                    return uptadeItem;
-                }
-            }
 
-        } catch (RuntimeException e) {
+        if (!itemRepository.getItems().containsKey(userId)) {
             throw new NotFoundException("невозможно обновить, пользователя с этим номером не существует ");
         }
-        throw new NotFoundException("невозможно обновить, т.к. пользователя с этим номером не существует ");
+
+        List<Item> clientItems = itemRepository.getItems().get(userId);
+        for (int k = 0; k < clientItems.size(); k++) {
+            if (clientItems.get(k).getId().equals(item.getId())) {
+                Item uptadeItem = clientItems.get(k);
+                if (item.getName() != null && item.getName() != uptadeItem.getName()) {
+                    uptadeItem.setName(item.getName());
+                }
+                if (item.getAvailable() != null && item.getAvailable() != uptadeItem.getAvailable()) {
+                    uptadeItem.setAvailable(item.getAvailable());
+                }
+                if (item.getDescription() != null && item.getDescription() != uptadeItem.getDescription()) {
+                    uptadeItem.setDescription(item.getDescription());
+                }
+                return uptadeItem;
+            }
+        }
+        throw new NotFoundException("невозможно обновить, т.к. итема с этим номером не существует ");
     }
 
     public Item findItemById(Long id) {

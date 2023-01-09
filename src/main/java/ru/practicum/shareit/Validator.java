5c937496ad0b71904af7a586e8198de8616aec68
Slava <svyatoslav.rudakas@yandex.ru>
2023-01-09 21:42:09 +0300

    correctStile5

--- src/main/java/ru/practicum/shareit/Validator.java
+++ src/main/java/ru/practicum/shareit/Validator.java
@@ -31,7 +31,7 @@ public class Validator {
 
     public void validateItemEmptyDescription(Item item) {
         if (item.getDescription() == null) {
-            throw new NoItemNameException("нужно указать описание");
+            throw new NoItemNameException("нужно указать описание вещи");
         }
     }
 
